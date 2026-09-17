c9b22a88fa49eb4efd64a263e3a5f27fcf4b5cef
HongPhuc2511 <hoanghongphucgl123@gmail.com>
2026-09-17 16:07:58 +0700

    feat:khởi tạo trang admin và superuser và cấu hình swagger để xem api của dự án #1

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import Course, Lesson, Vocabulary, Kanji, Grammar, Quiz, Question, Answer, QuizResult, UserProgress
+
+admin.site.register(Course)
+admin.site.register(Lesson)
+admin.site.register(Vocabulary)
+admin.site.register(Kanji)
+admin.site.register(Grammar)
+admin.site.register(Quiz)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(QuizResult)
+admin.site.register(UserProgress)
 # Register your models here.
